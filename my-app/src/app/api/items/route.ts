3c1fc3831aeef6f7b5abaea4c85119e4acb38a0d
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-05 17:01:20 +0900

    디비 연동

--- my-app/src/app/api/items/route.ts
+++ my-app/src/app/api/items/route.ts
@@ -1,5 +1,28 @@
 import { NextResponse } from 'next/server'
  
+const mysql = require('mysql2');  
+// const mysql = require('mysql2'); for MAC  
+  
+const connection = mysql.createConnection({  
+	host: process.env.DB_HOST,  
+	// host: '127.0.0.1', for MAC  
+	user: process.env.DB_USER,  
+	password: process.env.DB_PASSWORD,  
+	database: process.env.DB  
+});
+
+
+function queryPromise(queryString:string) {  
+	return new Promise((resolve, reject) => {  
+		connection.query(queryString, (error:any, results:any) => {  
+			if (error) {  
+				return reject(error);  
+			}  
+			resolve(results);  
+		});  
+	});  
+}  
+
 export async function GET() {
 //   const res = await fetch('https://data.mongodb-api.com/...', {
 //     headers: {
@@ -8,7 +31,18 @@ export async function GET() {
 //     },
 //   })
 //   const data = await res.json()
- 
-  const data = {test:'test'}
-  return NextResponse.json({ data })
+  
+  let queryString = 'select * from user';
+
+  console.log('?????')
+  try {  
+		const rows = await queryPromise(queryString);  
+    console.log(rows)
+		return NextResponse.json({data:rows , status:200});  
+	} catch (error) {  
+		console.error(error);  
+		return NextResponse.json('Internal Server Error');  
+	}  
+  // const data = {test:'test'}
+  // return NextResponse.json({ data })
 }
