f127ae3dcdc9b098102e07e495b3383cc5d60689
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-12 16:26:46 +0900

    start

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-   <>
+   <main style={{marginLeft:'0.5rem'}}>
     <h2>Welcome page!</h2>
-    Hello WEB
-   </>
+      Hello WEB
+      <button onClick={() =>router.push('/Sign/login')}>로그인</button>
+   </main>
   )
 }
