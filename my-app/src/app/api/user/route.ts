b9fbfd0e8a31447d740142e713ba09f4a407bf42
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-12 16:55:42 +0900

    fuck

--- my-app/src/app/api/user/route.ts
+++ my-app/src/app/api/user/route.ts
@@ -2,6 +2,8 @@ import { NextResponse , NextRequest} from 'next/server'
 
 const connection = require('../config/db');
 
-export async function POST() {
-    console.log(NextRequest.toString);
+export async function POST(request: Request) {
+    const {msg} = await request.json();
+    console.log(msg)
+    return msg
 }
