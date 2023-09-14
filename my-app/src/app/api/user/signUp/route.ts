e5fcc9d3f860388c314b66b05cc89bae5734cb09
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-14 17:11:51 +0900

    queryFunc 모듈화

--- my-app/src/app/api/user/signUp/route.ts
+++ my-app/src/app/api/user/signUp/route.ts
@@ -1,17 +1,7 @@
 import { NextResponse , NextRequest} from 'next/server'
+import { queryPromise } from '../../config/queryFunc';
 
-const connection = require('../../config/db');
 
-function queryPromise(queryString:string) {
-    return new Promise((resolve, reject) => {  
-		connection.query(queryString, (error:any, results:any) => {  
-			if (error) {  
-				return reject(error);  
-			}  
-			resolve(results);  
-		});  
-	});  
-}
 export async function POST(request:Request){
     const body = await request.json();
 
