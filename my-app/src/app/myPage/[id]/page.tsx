5eaf243aea921a64a13f0eda8635aa43da35ef91
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-25 14:06:17 +0900

    add button

--- my-app/src/app/myPage/[id]/page.tsx
+++ my-app/src/app/myPage/[id]/page.tsx
@@ -4,53 +4,70 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useRouter } from 'next/navigation';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import { grey } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
+import EditIcon from '@mui/icons-material/Edit';
 
 interface userData {
-    userId:string,
-    myIntro:string,
-    id:number,
-    userName:string,
-    userBirth:Date
+    userId: string,
+    myIntro: string,
+    id: number,
+    userName: string,
+    userBirth: Date
 }
 
-export default function MyPage()  {
-    const [cookies,setCookie, removeCookie] = useCookies(['userData']);
-    const [user,setUser] = useState<userData>();
+export default function MyPage() {
+    const [cookies, setCookie, removeCookie] = useCookies(['userData']);
+    const [user, setUser] = useState<userData>();
     const router = useRouter();
 
     useEffect(() => {
         let userData = cookies.userData;
-        if( !userData ){
+        if (!userData) {
             alert('로그인이 필요한 서비스입니다');
             router.push('/Sign/login');
-        }else{
+        } else {
             setUser(userData);
         }
-    },[]);
+    }, []);
     return (
         <>
-            <header style={{background:'#BDBDBD' , height:'13rem',padding:'1rem'}}>
-                <div style={{width:'7rem',height:'7rem',borderRadius:'70%',overflow:'hidden',float:'left'}}>
-                    <img style={{width:'100%',height:'100%', objectFit:'cover'}} src='https://texttokbucket.s3.ap-northeast-2.amazonaws.com/5875129.png'/>
+            <header style={{ background: '#BDBDBD', height: '13rem', padding: '1rem' }}>
+                <div style={{ width: '7rem', height: '7rem', borderRadius: '70%', overflow: 'hidden', float: 'left' }}>
+                    <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src='https://texttokbucket.s3.ap-northeast-2.amazonaws.com/5875129.png' />
 
                 </div>
-                <div className='profile' style={{float:'left',marginLeft:'2rem',width:'10rem'}} >
+                <div className='profile' style={{ float: 'left', marginLeft: '2rem', width: '10rem' }} >
                     <h1>{user?.userName}</h1>
                     <span>{user?.userId}</span>
                 </div>
-                <div className='hamadi' style={{display:'inline-block',width:'100%', marginTop:'1.5rem'}}>
+                <div className='hamadi' style={{ display: 'inline-block', width: '100%', marginTop: '1.5rem' }}>
                     {user?.myIntro}
                 </div>
             </header>
 
-            <div className='Tab' style={{background:'yellow', height:'3rem'}}>
+            <div className='Tab' style={{ background: 'yellow', height: '3rem' }}>
                 내 글, 친구 목록, 그룹 목록 탭
             </div>
 
-            <div style={{background:'green'}}>
+            <div style={{ background: 'green' }}>
                 board
                 <button onClick={() => console.log(cookies)}>test</button>
             </div>
+
+            <Box>
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    onClick={()=>{router.push('/myPage/write')}}
+                    sx={{ position: 'fixed', top: '80%', right: '16px',color:grey[900] }}
+                    icon={<EditIcon fontSize='small' sx={{color:grey}}/>}
+                    style={{color:'black'}}
+                >
+                </SpeedDial>
+            </Box>
+       
         </>
     )
 }
