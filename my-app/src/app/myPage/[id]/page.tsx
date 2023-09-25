348abaa2cac95e825d78e2c74c050ba07acd9904
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-09-25 14:19:09 +0900

    add tabs

--- my-app/src/app/myPage/[id]/page.tsx
+++ my-app/src/app/myPage/[id]/page.tsx
@@ -7,8 +7,10 @@ import { useRouter } from 'next/navigation';
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import { grey } from '@mui/material/colors';
-import Icon from '@mui/material/Icon';
 import EditIcon from '@mui/icons-material/Edit';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
 
 interface userData {
     userId: string,
@@ -18,11 +20,51 @@ interface userData {
     userBirth: Date
 }
 
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+
 export default function MyPage() {
     const [cookies, setCookie, removeCookie] = useCookies(['userData']);
     const [user, setUser] = useState<userData>();
     const router = useRouter();
 
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
     useEffect(() => {
         let userData = cookies.userData;
         if (!userData) {
@@ -48,26 +90,44 @@ export default function MyPage() {
                 </div>
             </header>
 
-            <div className='Tab' style={{ background: 'yellow', height: '3rem' }}>
-                내 글, 친구 목록, 그룹 목록 탭
+            <div className='Tab' style={{  height: '3rem',textAlign:'center' }}>
+                <Box sx={{ width: '100%' }}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered variant="fullWidth" >
+                            <Tab label="My list" {...a11yProps(0)} />
+                            <Tab label="Friends" {...a11yProps(1)} />
+                            <Tab label="Groups" {...a11yProps(2)} />
+                        </Tabs>
+                    </Box>
+
+                    <CustomTabPanel value={value} index={0}>
+                        Item One
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={1}>
+                        Item Two
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={2}>
+                        Item Three
+                    </CustomTabPanel>
+                </Box>
             </div>
 
-            <div style={{ background: 'green' }}>
+            {/* <div style={{ background: 'green' }}>
                 board
                 <button onClick={() => console.log(cookies)}>test</button>
-            </div>
+            </div> */}
 
             <Box>
                 <SpeedDial
                     ariaLabel="SpeedDial basic example"
-                    onClick={()=>{router.push('/myPage/write')}}
-                    sx={{ position: 'fixed', top: '80%', right: '16px',color:grey[900] }}
-                    icon={<EditIcon fontSize='small' sx={{color:grey}}/>}
-                    style={{color:'black'}}
+                    onClick={() => { router.push('/myPage/write') }}
+                    sx={{ position: 'fixed', top: '80%', right: '16px', color: grey[900] }}
+                    icon={<EditIcon fontSize='small' sx={{ color: grey }} />}
+                    style={{ color: 'black' }}
                 >
                 </SpeedDial>
             </Box>
-       
+
         </>
     )
 }
