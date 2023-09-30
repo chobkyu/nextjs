4bf48bbc041a14363995ae3f3ef8d7d73288cfae
chobkyu <qudqud97@naver.com>
2023-09-30 16:17:47 +0900

    update

--- my-app/src/app/myPage/[id]/FriendList.tsx
+++ my-app/src/app/myPage/[id]/FriendList.tsx
@@ -1,6 +1,40 @@
 "use client"
 
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+
+interface friendList{
+    user:number,
+    userId: string,
+    userName:string,
+    friend:number,
+    friendId:string,
+    friendName:string,
+}
+
 export function FriendList () {
+    const [cookies, setCookie, removeCookie] = useCookies(['userData']);
+    const [friendList,setFriendList] = useState<friendList[]>([]);
+
+    const getList = async () => {
+        const userId = cookies.userData.id;
+
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/board/getFriends/${userId}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setFriendList(res.data);
+            });
+    }
+
+    useEffect(()=>{
+        getList();
+    },[]);
+
+    const frinedListComponent = () => {
+        
+    }
+
     return (
         <div>
 
