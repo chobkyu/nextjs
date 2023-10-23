3b95b31eb7c848492c43b35bc62d92848b24bc64
chobkyu <qudqud97@naver.com>
2023-10-23 10:29:15 +0900

    test

--- my-app/src/app/read/[id]/page.tsx
+++ my-app/src/app/read/[id]/page.tsx
@@ -3,7 +3,7 @@ export default async function Read(props:any) {
     // const res = await fetch(`http://localhost:9999/topics/${props.params.id}`,{cache:'no-store'});
     // const topic = await res.json();
     return (
-        <>
+        <> 
             <h1>test</h1>
             {/* <h2>{topic.title}</h2>
             {topic.body} */}
